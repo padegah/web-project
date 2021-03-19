e667a7e29593b4b65e9ce909c33f2fba128fae50
prince adegah <adegah.prince@yahoo.com>
2021-03-19 02:36:21 +0000

    updated sliders on home page

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000);
+  setTimeout(carousel, 3000);
 }
 /*
 slide behaviour ends here
